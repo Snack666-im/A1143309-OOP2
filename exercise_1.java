3c7e3ccf8fa29c5ebe2bc992ecac34f488c9d254
Snack666-im <arknightjed@gmail.com>
2026-09-20 21:09:14 +0800

    Create exercise_1.java

---
+++ exercise_1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class exercise_1{
+	public static void main(String args[]){
+		JFrame frm = new JFrame("snack的JFrame視窗") ;
+		frm.setSize(500,300);
+		frm.setLocation(0,0);
+		frm.setVisible(true);
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
+	
